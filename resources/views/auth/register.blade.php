be37734fc98665ce0a39c20d720e5c90df89902f
youkoucoding <youkousai@gmail.com>
2021-03-29 23:09:35 +0900

    register blade

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app')
+
+@section('dynamic_content')
+    <div class="flex flex-wrap justify-center items-center font-mono bg-white pt-24 rounded-lg">
+
+        <div class="w-4/12">
+            <form action="{{ URL('/register') }}" method="POST" >
+                @csrf
+                <div class="bg-green-300 mb-4 p-4 text-center rounded-2xl ">
+                    <h1 class="uppercase text-lg">Please Register</h1>
+                </div>
+
+                {{-- user name  --}}
+                <div class="mb-4">
+                    <input type="text" name="name"
+                           class="bg-gray-100 border-2 w-full p-4 rounded-lg"
+                           placeholder="User Name" value="{{ old('uname')}}">
+
+                    @error('name')
+                    <div>
+                        <h1 class="text-red-300">Name is required|longer</h1>
+                    </div>
+                    @enderror
+
+                </div>
+
+                {{-- email address  --}}
+                <div class="mb-4">
+                    <input type="email" name="email"
+                           class="bg-gray-100 border-2 w-full p-4 rounded-lg"
+                           placeholder="Email Address" value="{{ old('email')}}">
+
+
+                    @error('email')
+                    <div>
+                        <h1 class="text-red-300">Please enter valid email address</h1>
+                    </div>
+                    @enderror
+                </div>
+
+                {{-- phone number  --}}
+                <div class="mb-4">
+                    <input type="text" name="mobile"
+                           class="bg-gray-100 border-2 w-full p-4 rounded-lg"
+                           placeholder="phone number" value="{{ old('mobile')}}">
+
+
+                    @error('mobile')
+                    <div>
+                        <h1 class="text-red-300">Please enter valid email address</h1>
+                    </div>
+                    @enderror
+                </div>
+
+                {{-- password  --}}
+                <div class="mb-4">
+                    <input type="password" name="password"
+                           class="bg-gray-100 border-2 w-full p-4 rounded-lg"
+                           placeholder="Password">
+
+
+                    @error('password')
+                    <div>
+                        <h1 class="text-red-300">Please enter valid password</h1>
+                    </div>
+                    @enderror
+                </div>
+
+                {{-- Confirm Password  --}}
+                <div class="mb-4">
+                    <input type="password" name="password_confirmation"
+                           class="bg-gray-100 border-2 w-full p-4 rounded-lg"
+                           placeholder="Confirm Password">
+
+
+                    @error('password_confirmation')
+                    <div>
+                        <h1 class="text-red-300">Password does not match</h1>
+                    </div>
+                    @enderror
+                </div>
+
+                {{-- Submit  --}}
+                <div class="mb-0 flex justify-center">
+                    <input type="submit" name="submit" value="SUBMIT" class="bg-yellow-500 border-2 p-4 rounded-lg">
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,31 @@
 <!doctype html>
+<html>
 <head>
-    <!-- ... --->
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Laravel8-authentication-demo</title>
 </head>
-<body class="bg-gray-200">
+<body>
 
-    <nav class='flex justify-between items-center h-16 font-mont shadow-sm relative text-black bg-white' role='navigation'>
-        <a href="/" class='pl-8'>Navbar</a>
-        <div class='px-4 cursor-pointer md:hidden' onClick={{}}>
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </div>
-        <div class="pr-8 md:block hidden">
-            <a href="{{URL('/')}}" class='p-4'>Home</a>
-            <a href="{{}}" class='p-4'>Menu</a>
-            <a href="{{}}" class='p-4'>About</a>
-            <a href="{{}}" class='p-4'>Contact</a>
-        </div>
-    </nav>
+<nav class='flex justify-between items-center h-16 font-mono bg-gray-100 font-bold  relative text-black bg-white' role='navigation'>
+    <a href="/" class='pl-8'>Navbar</a>
+    <div class='px-4 cursor-pointer md:hidden'>
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+    </div>
+    <div class="pr-8 md:block hidden">
+        <a href="{{URL("/")}}" class='p-4'>Home</a>
+        <a href="{{URL("/")}}" class='p-4'>Menu</a>
+        <a href="{{URL("/")}}" class='p-4'>About</a>
+        <a href="{{URL("/")}}" class='p-4'>Contact</a>
+    </div>
+</nav>
 
+{{--template inhertence--}}
+<div class="mb-0 pb-0">
+    @yield("dynamic_content")
+</div>
 </body>
+</html>
