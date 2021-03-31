82987ca74b241fc88c86fe9151f91f8ef3869236
youkoucoding <youkousai@gmail.com>
2021-03-31 12:32:06 +0900

    middleware of AuthCheck

--- app/Http/Middleware/AuthCheck.php
+++ app/Http/Middleware/AuthCheck.php
@@ -11,11 +11,21 @@ class AuthCheck
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Closure                  $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (!session()->has('LoggedUser') && ($request->path() != 'auth/login' && $request->path() != 'auth/register')) {
+            return redirect('auth/login')->with('fail', 'You must be login first.');
+        }
+
+        if (session()->has('LoggedUser') && ($request->path() == 'auth/login' || $request->path() == 'auth/register')) {
+            return back();
+        }
+
+        return $next($request)->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', 'Sat 01 Jan 1990 00:00:00 GMT');
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,21 +12,26 @@
                 class="absolute z-10 inset-0 bg-gradient-to-r from-yellow-300 via-red-500 to-blue-500 shadow-lg transform -skew-x-6 sm:skew-x-0 sm:-rotate-6 sm:rounded-lg"
             ></div>
             <div class="relative z-20 bg-white rounded-md shadow-md p-12">
-               <table class="grid grid-cols-6 space-y-6 space-x-3">
-                   <thread>
-                       <th>Name</th>
-                       <th>Email</th>
-                       <th>Mobile</th>
-                   </thread>
-                   <tbody>
-                   <tr>
-                       <td>{{$LoggedUserInfo['name']}}</td>
-                       <td>{{$LoggedUserInfo['email']}}</td>
-                       <td>{{$LoggedUserInfo['mobile']}}</td>
-                       <td><a href="{{route('auth.logout')}}">LogOut</a></td>
-                   </tr>
-                   </tbody>
-               </table>
+                <div class="flex items-center justify-between flex-cols gap-x-10">
+                    <table class="table-fixed">
+                        <thead class="m-3 px-10">
+                        <tr>
+                            <th class="w-1/2">Name</th>
+                            <th class="w-1/3">Email</th>
+                            <th class="w-1/3">Mobile</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr class="bg-emerald-200">
+                            <td class="w-1/2">{{$LoggedUserInfo['name']}}</td>
+                            <td class="w-1/3">{{$LoggedUserInfo['email']}}</td>
+                            <td class="w-1/3">{{$LoggedUserInfo['mobile']}}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <a href="{{route('auth.logout')}}"
+                       class="inline-block py-3 px-4 bg-gray-400 font-bold tracking-wider rounded-md ">Logout</a>
+                </div>
             </div>
         </div>
     </section>
